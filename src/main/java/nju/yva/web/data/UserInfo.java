cf3d912719eadafc10405f2b1e498c202aa5f6bf
WaterWan <598127577@qq.com>
2017-08-11 16:55:13 +0800

    1.建立所有逻辑层的实体对象 2.更新了lombox的插件版本

---
+++ src/main/java/nju/yva/web/data/UserInfo.java
@@ -0,0 +1,24 @@
+package nju.yva.web.data;
+
+import lombok.Data;
+
+/**
+ * Created by Water on 2017/8/11.
+ */
+@Data
+public class UserInfo {
+    // 用户的id
+    private String id;
+    // 用户的名字
+    private String name;
+    // 用户的学号
+    private String studentNo;
+    // 用户的电话号码
+    private String telNum;
+    // 用户的志愿时长,显示用的直接String了
+    private String volunteerTime;
+    // 是否审核过
+    private boolean checked;
+    // 是否通过审核
+    private boolean passed;
+}
