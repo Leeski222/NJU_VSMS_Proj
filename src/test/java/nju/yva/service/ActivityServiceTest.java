4dd7fe9d3fe534928fdcc51aa54b1c48b83370fc
WaterWan <598127577@qq.com>
2017-08-19 16:45:54 +0800

    新增测试文件

---
+++ src/test/java/nju/yva/service/ActivityServiceTest.java
@@ -0,0 +1,15 @@
+package nju.yva.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by Water on 2017/8/19.
+ */
+@RunWith(MockitoJUnitRunner.class)
+@WebAppConfiguration
+@SpringBootTest
+public class ActivityServiceTest {
+}
