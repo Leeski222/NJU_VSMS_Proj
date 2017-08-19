9e66812ab978d1caf99b42a60ea24b31f605f318
WaterWan <598127577@qq.com>
2017-08-19 19:57:49 +0800

    新增User一些注解

---
+++ src/main/java/nju/yva/dao/UserDao.java
@@ -0,0 +1,11 @@
+package nju.yva.dao;
+
+import nju.yva.model.UserData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Water on 2017/8/19.
+ */
+public interface UserDao extends JpaRepository<UserData, Long> {
+    UserData findOneById(long id);
+}
--- src/main/java/nju/yva/model/UserData.java
+++ src/main/java/nju/yva/model/UserData.java
@@ -1,12 +1,36 @@
 package nju.yva.model;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by Water on 2017/8/19.
  */
+@Data
+@Entity
+@Table(name = "user_data")
 public class UserData {
+    @NotNull
+    @Column(name = "vol_hours")
     private double voluntaryHours;
+
+    @NotNull
+    @Column(name = "user_name")
     private String name;
+
+    @Id
     private long id;
+
+    @NotNull
+    @Column(name = "tel_nul")
     private String telephoneNum;
+
+    @NotNull
+    @Column(name = "stu_no")
     private String studentNo;
 }
